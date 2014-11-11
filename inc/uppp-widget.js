ea08f0a6d2549c3d2fe2dceb09cca2a3cc9bedb1
Samer Bechara <sam@courtix.com>
2014-11-11 11:53:20 +0200

    Removing ajax from frontend form

--- inc/uppp-widget.js
+++ inc/uppp-widget.js
@@ -8,27 +8,7 @@ jQuery(document).ready(function() {
      * Submit form when number of results is changed
      */
     jQuery('#num_results').change(function () {
-        
-        //jQuery('#uppp_form').submit();
-        
-        // Get current url
-        var link = jQuery(location).attr('href');
-        
-        jQuery('#container').fadeOut(500, function(){
-
-               // Get new posts
-                jQuery.post(ajaxurl, {
- 
-                    action:     'uppp_get_posts',
- 
- 
-                }, function (response) {
- 
-                    console.log(response);
- 
-                });
- 
-        });        
+        jQuery('#uppp_form').submit();
     });
     
 });
